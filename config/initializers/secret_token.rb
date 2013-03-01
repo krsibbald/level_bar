cd48a98bddb174636275b5268094414a09a0bb70
Katharine Rawson Sibbald <krsibbald@gmail.com>
2013-02-28 22:59:16 -0500

    Generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LevelBar::Application.config.secret_token = 'f31df7fdc37bfa4573289e4bc6685cca1ac2ee2f69315e2a57aa29b7d095efea48ebae7710ca452ea22b0273198fd2ce2222f4e61ac03030e01ab9ae4862e68f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LevelBar::Application.config.session_store :cookie_store, key: '_level_bar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LevelBar::Application.config.session_store :active_record_store
