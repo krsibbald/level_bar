3cd3948db302fb9e02d5ded4a2ccd356b861cb80
Katharine Rawson Sibbald <krsibbald@gmail.com>
2013-04-23 23:11:08 -0400

    Layout improvements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,37 @@
 class PagesController < ApplicationController
   def about
+    @tab = "about"
   end
   
   def apply
+    @tab = "apply"
   end
   
   def contact
+    @tab = "contact"
   end
   
   def donate
+    @tab = "donate"
   end
   
   def home
+    @tab = "home"
   end
   
   def leadership
+    @tab = "leadership"
   end
   
   def lsat
+    @tab = "lsat"
   end
   
   def mission
+    @tab = "mission"
   end
   
   def services
+    @tab = "services"
   end
 end
